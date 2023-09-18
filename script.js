2fc143dfa480d428f66a7a01e84306beb54fe252
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-09-18 21:33:33 +0700

    feat: make game fully functional

--- script.js
+++ script.js
@@ -2,4 +2,23 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+};
+
+const compareGuesses = (user, computer, target) => {
+  return Math.abs(user - target) <= Math.abs(computer - target);
+};
+
+const updateScore = (winner) => {
+  switch (winner) {
+    case "human":
+      humanScore++;
+    case "computer":
+      computerScore++;
+  }
+};
+
+const advanceRound = () => {
+  currentRoundNumber++;
+};
